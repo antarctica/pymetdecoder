3e4bc3f2043de002d5a9934bbb2da297e76b8062
Tim Barnes <tdba@bas.ac.uk>
2021-07-19 12:37:32 +0100

    updated setup.py and CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## 0.1.1 [2021-07-19]
+
+### Fixed
+
+* (#1) - Group 6 codes in section3 now properly decode when a group 5 code is present
+
 ## 0.1.0 [2021-05-27]
 
 * First version
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name             = "pymetdecoder",
-    version          = "0.1.0",
+    version          = "0.1.1",
     author           = "Tim Barnes",
     author_email     = "tdba@bas.ac.uk",
     description      = "Python module to decode/encode met reports e.g. SYNOPs",
